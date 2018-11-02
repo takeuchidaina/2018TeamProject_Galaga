3bdf97126597e574c3edaeb091822615e57f05ef
MellMag <yda.tm.2360623@gmail.com>
2018-11-02 11:18:39 +0900

    [add][fix] とりあえず動くようになります

---
+++ source/Template.cpp
@@ -0,0 +1,36 @@
+#include "Template.h"
+
+//Templateクラスの実装
+
+//コンストラクタ
+cTemplate::cTemplate(){
+	
+}
+
+//デストラクタ
+cTemplate::~cTemplate() {
+
+}
+
+int cTemplate::Update() {
+	return 0;
+}
+
+int cTemplate::Draw() {
+	return 0;
+}
+
+
+
+/*
+//実装例
+//mTestへの代入
+void cTemplate::Set_Test(int Test) {
+	mTest = Test;
+}
+
+//mTestの獲得
+int cTemplate::Get_Test() {
+	return mTest;
+}
+*/
