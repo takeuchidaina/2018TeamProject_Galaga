7b3d1ea6a070679f843f2521ceea08f59e719bed
takasiro <41959070+takasiro@users.noreply.github.com>
2018-11-04 17:58:14 +0900

    [update] enemyの動きをまともに
    継承元なので基本的な構造に修正。取り敢えず動くように。

--- source/cBaseEnemy.cpp
+++ source/cBaseEnemy.cpp
@@ -9,33 +9,9 @@ using namespace std;
 
 
  cBaseEnemy::cBaseEnemy(){
-	 enemy.pos.x = 700;
-	 enemy.pos.y = 450;
-	 enemy.r = 5;
-	 enemy.count = 0;
-	 enemy.spd = 3;
-	 enemy.ang = 180 * 3.14159265 / 180;
-	 enemy.onActive = 0;
-
-	 enemy.moveflg = 0;
-
-	 enemy.vct.x = 0;
-	 enemy.vct.y = 0;
-
-	 memset(enemy.moveang, 0, sizeof(enemy.moveang));
-	 enemy.moveang[0] = 0.5;
-	 enemy.moveang[1] = 3;
-
-	 memset(enemy.countflg, 0, sizeof(enemy.countflg));
-	 enemy.countflg[0] = 120;
-	 enemy.countflg[1] = 120;
-
-	 enemy.target.x = 160;
-	 enemy.target.y = 64;
-	 enemy.targetr = 1;
+	
 
 }
-
  cBaseEnemy::cBaseEnemy(double x,double y,double r,int cnt ,double spd ,double ang,int flg) {
 	 enemy.pos.x = x;
 	 enemy.pos.y = y;
@@ -59,7 +35,7 @@ using namespace std;
 
 	 memset(enemy.countflg, 0, sizeof(enemy.countflg));
 	 enemy.countflg[0] = 30;
-	 enemy.countflg[1] = 120;
+	 enemy.countflg[1] = 40;
 	 enemy.countflg[2] = 15;
 	 enemy.countflg[3] = 30;
 	 enemy.countflg[4] = 45;
@@ -70,21 +46,182 @@ using namespace std;
 	 
  }
 
+ cBaseEnemy::cBaseEnemy(cBaseEnemy &unit) {
+	 
+ }
+
+
 cBaseEnemy::~cBaseEnemy() {
 
 }
 
+void cBaseEnemy::Move(cBaseEnemy &unit) {
+	if (unit.enemy.dir == unit.enemy.RIGHT) {
+		unit.enemy.vct.x = cos(enemy.ang);
+		unit.enemy.vct.y = sin(enemy.ang);
+		unit.enemy.pos.x += unit.enemy.vct.x*unit.enemy.spd;
+		unit.enemy.pos.y += unit.enemy.vct.y*unit.enemy.spd;
+	}
+	else {
+		if (unit.enemy.moveflg != 5) {
+			unit.enemy.vct.x = -cos(enemy.ang);
+			unit.enemy.vct.y = sin(enemy.ang);
+			unit.enemy.pos.x += unit.enemy.vct.x*unit.enemy.spd;
+			unit.enemy.pos.y += unit.enemy.vct.y*unit.enemy.spd;
+		}
+		else {
+			unit.enemy.vct.x = cos(enemy.ang);
+			unit.enemy.vct.y = sin(enemy.ang);
+			unit.enemy.pos.x += unit.enemy.vct.x*unit.enemy.spd;
+			unit.enemy.pos.y += unit.enemy.vct.y*unit.enemy.spd;
+		}
+	}
+}
+
+
 int cBaseEnemy::Update() {
-	
+
+	enemy.count++;
+
+	if (enemy.count > 0) {
+		enemy.onActive = true;
+	}
+	if (enemy.RIGHT == enemy.dir) {
+		switch (enemy.moveflg)
+		{
+		case 0:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 1:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 2:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 3:
+			enemy.ang = 0;
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 4:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 5:
+			enemy.ang = atan2(enemy.target.y - enemy.pos.y, enemy.target.x - enemy.pos.x);
+			//Move(unit);
+			if ((enemy.target.x - enemy.pos.x)*(enemy.target.x - enemy.pos.x) *
+				(enemy.target.y - enemy.pos.y)*(enemy.target.y - enemy.pos.y) <=
+				(enemy.r + enemy.targetr)*(enemy.r + enemy.targetr)) {
+				//敵座標を目的地に固定
+				enemy.pos.x = enemy.target.x;
+				enemy.pos.y = enemy.target.y;
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 6:
+			enemy.count = 0;
+			enemy.moveflg = 0;
+			enemy.ang = 180 * 3.14159265 / 180;
+			enemy.dir *= -1;
+			break;
+
+		}
+	}
+	else {
+		switch (enemy.moveflg)
+		{
+		case 0:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 1:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 2:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 3:
+			enemy.ang = 0;
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//	Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 4:
+			enemy.ang += enemy.moveang[enemy.moveflg] * 3.1419265 / 180;
+			//Move(unit);
+			if (enemy.countflg[enemy.moveflg] <= enemy.count) {
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 5:
+			enemy.ang = atan2(enemy.target.y - enemy.pos.y, enemy.target.x - enemy.pos.x);
+			//Move(unit);
+			if ((enemy.target.x - enemy.pos.x)*(enemy.target.x - enemy.pos.x) *
+				(enemy.target.y - enemy.pos.y)*(enemy.target.y - enemy.pos.y) <=
+				(enemy.r + enemy.targetr)*(enemy.r + enemy.targetr)) {
+				//敵座標を目的地に固定
+				enemy.pos.x = enemy.target.x;
+				enemy.pos.y = enemy.target.y;
+				enemy.moveflg++;
+				enemy.count = 0;
+			}
+			break;
+		case 6:
+			enemy.count = 0;
+			enemy.moveflg = 0;
+			enemy.ang = 180 * 3.14159265 / 180;
+			enemy.dir *= -1;
+			break;
+
+		}
+	}
 	return 0;
 }
 
-void cBaseEnemy::Move(cBaseEnemy &unit) {
-	unit.enemy.vct.x = cos(enemy.ang);
-	unit.enemy.vct.y = sin(enemy.ang);
-	unit.enemy.pos.x += unit.enemy.vct.x*unit.enemy.spd;
-	unit.enemy.pos.y += unit.enemy.vct.y*unit.enemy.spd;
-}
+
 
 int cBaseEnemy::Draw() {
 	
