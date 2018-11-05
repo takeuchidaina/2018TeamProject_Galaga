4dbe69155b309195e03e1444caff8c922aa9d9ec
takasiro <41959070+takasiro@users.noreply.github.com>
2018-11-05 09:56:43 +0900

    [add] 三個目の継承Enemyの追加
    ボスギャラガ用のクラスの作成

--- source/cGreenEnemy.h
+++ source/cGreenEnemy.h
@@ -1,1 +1,15 @@
 #pragma once
+#ifndef GREENENEMY_INCLUDE
+#define GREENENEMY_INCLUDE
+
+#include "cBaseEnemy.h"
+
+
+class cGreenEnemy : public cBaseEnemy {
+public:
+	cGreenEnemy(double, double, double, int, double, double, int);
+	virtual void Move(cGreenEnemy &);
+	virtual int Update();
+	virtual int Draw();
+};
+#endif // !REDENEMY_INCLUDE
