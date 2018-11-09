aae95b388108a25db3214796ff994b4cef300dc3
takeuchidaina <yda.takeuchidaina@gmail.com>
2018-11-09 10:34:09 +0900

    [add] UI.cppとUI.hの追加
    UIを作成する際の基盤を追加しました。

---
+++ source/UI.cpp
@@ -0,0 +1,26 @@
+#include <DxLib.h>
+#include "UI.h"
+
+int UI_Init()
+{
+
+	return 0;
+}
+
+int UI_Update()
+{
+
+	return 0;
+}
+
+int UI_Draw()
+{
+
+	return 0;
+}
+
+int UI_End()
+{
+
+	return 0;
+}
---
+++ source/UI.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _UI_INCLUDE_
+#define _UI_INCLUDE_
+
+#include "Struct.h"
+
+extern int UI_Init();
+extern int UI_Update();
+extern int UI_Draw();
+extern int UI_End();
+
+#endif
