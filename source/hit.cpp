9706ba3e94bcb11a64a1ae6e9180fb13a75cc9a1
Yukiya1027 <yukiya1027yda@gmail.com>
2018-11-27 14:03:09 +0900

    [update] 残るはEnemy情報のみ！！
    PlayerとShotの情報導入完了

--- source/hit.cpp
+++ source/hit.cpp
@@ -41,10 +41,15 @@ void cHit::Draw() {
 ************************************************************/
 void cHit::Hit() {
 
+	
+
 	sOBJPos tmpPlayer[2];
-	cShot tmpEShot[20];
-	cShot tmpPShot[2];
+	cShot *tmpEShot[20];
+	cShot *tmpPShot[2];
 	sEnemy tmpEnemy[20];
+
+	tmpPShot[0] = cShotMgr::Instance()->GetShot(PLAYER);
+	tmpEShot[0] = cShotMgr::Instance()->GetShot(ENEMY);
 
 
 	/* 自機と敵弾 */
@@ -52,15 +57,13 @@ void cHit::Hit() {
 
 		if (tmpPlayer[i].onActive == FALSE) continue;
 
-
-
 		for (int j = 0; j < sizeof(tmpEShot); j++) {
 
-			if (tmpEShot[j].Get_OnActive == FALSE) continue;
+			if (tmpEShot[j]->Get_OnActive == FALSE) continue;
 
-			int len = ( (tmpEShot[j].Get_ShotCX - tmpPlayer[i].cx) * (tmpEShot[j].Get_ShotCX - tmpPlayer[i].cx) ) + ( (tmpEShot[j].Get_ShotCY - tmpPlayer[i].cy)*(tmpEShot[j].Get_ShotCY - tmpPlayer[i].cy) );
+			int len = ( (tmpEShot[j]->Get_ShotCX - tmpPlayer[i].cx) * (tmpEShot[j]->Get_ShotCX - tmpPlayer[i].cx) ) + ( (tmpEShot[j]->Get_ShotCY - tmpPlayer[i].cy)*(tmpEShot[j]->Get_ShotCY - tmpPlayer[i].cy) );
 
-			if ( len <= ((tmpEShot[j].Get_ShotR + tmpPlayer[i].r) * (tmpEShot[j].Get_ShotR + tmpPlayer[i].r)) ) {
+			if ( len <= ((tmpEShot[j]->Get_ShotR + tmpPlayer[i].r) * (tmpEShot[j]->Get_ShotR + tmpPlayer[i].r)) ) {
 
 				/* プレイヤーが2機いるときに片方がダウン
 				   呼び出し関数：cPlayer::Braik("プレイヤーの状態", "どっちのプレイヤーか") */
@@ -95,7 +98,7 @@ void cHit::Hit() {
 				/* プレイヤーが2機いるときに片方がダウン
 				呼び出し関数：cPlayer::Braik("プレイヤーの状態", "どっちのプレイヤーか") *
 				if (tmpPlayer[eLeftMachine].onActive == TRUE && tmpPlayer[eRightMachine].onActive == TRUE) {
-					cPlayer::Instance()->Break(eDoubleDeath, i);
+					cPlayer::Instance()->Break(eDeath, i);
 					sEnemy::Instance()->Break(j);
 				}
 
@@ -116,16 +119,16 @@ void cHit::Hit() {
 
 		for (int j = 0; j < sizeof(tmpPShot); j++) {
 
-			if (tmpPShot[j].Get_OnActive == FALSE) continue;
+			if (tmpPShot[j]->Get_OnActive == FALSE) continue;
 
-			int len = (tmpPShot[j].Get_ShotCX - enemy[i].cx)*(tmpPShot[j].Get_ShotCX - enemy[i].cx) + (tmpPShot[j].Get_ShotCY - enemy[i].cy)*(tmpPShot[j].Get_ShotCY - enemy[i].cy);
+			int len = (tmpPShot[j]->Get_ShotCX - enemy[i].cx)*(tmpPShot[j]->Get_ShotCX - enemy[i].cx) + (tmpPShot[j]->Get_ShotCY - enemy[i].cy)*(tmpPShot[j]->Get_ShotCY - enemy[i].cy);
 
-			if (len <= ((enemy[i].r + tmpPShot[j].Get_ShotR)*(enemy[i].r + tmpPShot[j].Get_ShotR)) ) {
+			if (len <= ((enemy[i].r + tmpPShot[j]->Get_ShotR)*(enemy[i].r + tmpPShot[j]->Get_ShotR)) ) {
 				cShotMgr::Instance()->Break(PLAYER, j);
 				sEnemy::Instance()->Break(i);
 			}
 		}
-	}*/
+	}
 }
 
 /************************************************************
