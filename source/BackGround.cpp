d987bb553e40bb90e8bfee897945502c615b7d61
Yukiya1027 <yukiya1027yda@gmail.com>
2018-12-14 15:06:34 +0900

    [update] 画像を使った背景は完成

--- source/BackGround.cpp
+++ source/BackGround.cpp
@@ -8,9 +8,12 @@ cBackGround::cBackGround() {
 		Init(i);
 	}
 
+	srand((unsigned int)time(NULL)); // 現在時刻の情報で初期化
+
+	image = LoadGraph("../resource/Image/Star.png");
 }
 
-/* デストラクタ *
+/* デストラクタ 
 cBackGround::~cBackGround() {
 
 }*/
@@ -20,13 +23,17 @@ void cBackGround::Update() {
 	for (int i = 0; i < MAXSTAR; i++) {
 
 		/* 星を流す */
-		starArray[i].y += 10;
-		if (starArray[i].y >= DISP_SIZE) Init(i);
+		starArray[i].y += starArray[i].speed;
+		if (starArray[i].y >= DISP_SIZE + 50) {
+			starArray[i].y = (rand() % 100) - 100;
+			starArray[i].x = rand() % DISP_SIZE;
+		}
 
 		/* 星を点滅 */
 		starArray[i].blinkCnt++;
 		if (starArray[i].blinkCnt >= starArray[i].blinkNum) {
 			starArray[i].blinkFlg *= -1;
+			starArray[i].blinkCnt = 0;
 		}
 
 	}
@@ -36,8 +43,8 @@ void cBackGround::Draw() {
 
 	for (int i = 0; i < MAXSTAR; i++) {
 		if (starArray[i].blinkFlg == TRUE) {
-			LoadGraphScreen((int)starArray[i].x, (int)starArray[i].y, 
-				"../resource/Image/Star.png", TRUE);
+			DrawGraph((int)starArray[i].x, (int)starArray[i].y,
+				image, TRUE);
 		}
 	}
 }
@@ -50,24 +57,11 @@ void cBackGround::Draw() {
 ***************************************************************/
 void cBackGround::Init(int i) {
 
-		starArray[i].x = Rand(0, DISP_SIZE);	//ｘ座標
-		starArray[i].y = Rand(-100, 300);		//ｙ座標
-		starArray[i].blinkCnt = 0;				//点滅カウント用
-		starArray[i].blinkNum = Rand(10, 100);	//点滅タイミング
-		starArray[i].blinkFlg = TRUE;			//TRUE：表示　FALSE：非表示
+	starArray[i].x = rand() % DISP_SIZE;			//ｘ座標
+	starArray[i].y = (rand() % DISP_SIZE) - 100;	//ｙ座標
+	starArray[i].speed = 2;							//移動速度
+	starArray[i].blinkCnt = 0;						//点滅カウント用
+	starArray[i].blinkNum = (rand() % 100) +10;		//点滅タイミング
+	starArray[i].blinkFlg = TRUE;					//TRUE：表示　FALSE：非表示
 
-}
-/***************************************************************
-	関数：int cBackGround::Rand(int lo, int hi)
-	説明：ランダム処理を行う
-	引数：ランダム範囲の低数(int), ランダム範囲(int)
-	戻値：ランダム結果(int)
-***************************************************************/
-int cBackGround::Rand(int lo, int hi) {
-
-	int r = 0;
-	srand((unsigned int)time(NULL)); // 現在時刻の情報で初期化
-	r = (rand() % hi) + lo;
-
-	return r;
 }
