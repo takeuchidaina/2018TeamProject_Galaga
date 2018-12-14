e080cbd8a5c0677882dc77fe35a2c39609649f1d
takeuchidaina <yda.takeuchidaina@gmail.com>
2018-12-14 11:54:14 +0900

    [update]【Score】リザルトの作成
    '-' と'%'の文字が対応していないためそこは表示されません。
    score表示も桁に応じてずれるように対応予定。
    位置調整もします。

--- source/Score.h
+++ source/Score.h
@@ -8,9 +8,13 @@ class cScore : public cSingleton <cScore> {
 	cScore();
 	friend cSingleton <cScore>;
 private:
-	int score;
+	int score;			  //スコア
+	int totalShot;		  //弾を撃った数
+	int totalHit;		  //弾を当てた数
+	float hitMissRatio;	  //弾を外した割合
 public:
 	void Draw();
+	void Result();
 
 	void AddScore(int n) { score += n; }
 	void ResetScore() { score = 0; }
